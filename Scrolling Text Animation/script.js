96bd8b2de3f703ef75570161686ea7b4c850eaaa
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-01 18:44:14 +0530

    Added Scrolling Text Animations Task.

---
+++ Scrolling Text Animation/script.js
@@ -0,0 +1,25 @@
+window.addEventListener("wheel", function (dets) {
+  if (dets.deltaY > 0) {
+    gsap.to(".marque", {
+      transform: "translateX(-200%)",
+      duration: 5,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(".marque img", {
+      rotate: 180
+    });
+  } else {
+    gsap.to(".marque", {
+      transform: "translateX(0%)",
+      duration: 5,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(".marque img", {
+      rotate: 0
+    });
+  }
+});
